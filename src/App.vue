700267998bfe17f6cbddb1caf41c9465aa175928
matalabanana <mathieulesteun@gmail.com>
2023-04-10 07:26:02 +0200

    correction

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-
   <div> 
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/"> Vue défaut </router-link> |
       <router-link to="/about">About</router-link> | 
       <router-link to="/produit/chocolat">Chocolat </router-link> | 
-      <router-link to="/shoppingCard">Panier </router-link>
+      <router-link to="/shoppingCard">Panier </router-link> | 
+      <router-link to="/catalogue">Catalogue </router-link>
     </nav>
 
+    <div class="alert alert-secondary">
     <router-view/>
+    </div> 
+
+        <button class="btn btn-primary" 
+            data-bs-target="#collapseTarget" 
+            data-bs-toggle="collapse">
+            Bootstrap collapse
+        </button>
+        <div class="collapse py-2" id="collapseTarget">
+            This is the toggle-able content!
+        </div>
+
+
   </div>
 </template>
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p> Je suis uniqument dans views/AboutView.vue car je suis une page statique </p>
+
+    <p> </p> 
   </div>
 </template>
